ab0aea5cbf8fbe5844005266cc1749d64adff650
JeeSeong <rhtmxm17@gmail.com>
2024-09-26 12:45:34 +0900

    feat: 플레이어를 추적해서 이동 및 공격 가능한지 판정

--- Assets/Scripts/HumanoidMonster.cs
+++ Assets/Scripts/HumanoidMonster.cs
@@ -1,7 +1,9 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AI;
 
+[RequireComponent(typeof(NavMeshAgent))]
 public class HumanoidMonster : MonoBehaviour
 {
     private enum State { Idle, Chase, Attack }
@@ -10,11 +12,17 @@ public class HumanoidMonster : MonoBehaviour
     private class Shared
     {
         public readonly YieldInstruction detectPeriod = new WaitForSeconds(1f);
-        public readonly LayerMask playerLayer = LayerMask.GetMask("Player");
+        public readonly YieldInstruction checkAttackPeriod = new WaitForSeconds(1f);
+        public readonly LayerMask playerLayerMask = LayerMask.GetMask("Player");
+        public readonly LayerMask hitableLayerMask = LayerMask.GetMask("Player", "Default");
+        public readonly int playerLayerIndex = LayerMask.NameToLayer("Player");
     }
     private Shared shared;
 
     [SerializeField] float detectRange = 10f;
+    [SerializeField] float attackRange = 5f;
+
+    private NavMeshAgent agent;
 
     [SerializeField] // 확인용
     private State currentState = State.Idle;
@@ -25,6 +33,7 @@ public class HumanoidMonster : MonoBehaviour
     private void Awake()
     {
         shared = new();
+        agent = GetComponent<NavMeshAgent>();
     }
 
     private void Start()
@@ -46,11 +55,35 @@ public class HumanoidMonster : MonoBehaviour
     {
         while (true)
         {
-            if (0 < Physics.OverlapSphereNonAlloc(transform.position, detectRange, detected, shared.playerLayer))
+            if (0 < Physics.OverlapSphereNonAlloc(transform.position, detectRange, detected, shared.playerLayerMask))
             {
-                Debug.Log("감지됨");
+                currentState = State.Chase;
+                StopCoroutine(currentRoutine);
+                currentRoutine = StartCoroutine(ChasePlayerRoutine());
+                agent.destination = detected[0].transform.position;
             }
+
             yield return shared.detectPeriod;
+        }
+    }
+
+    private IEnumerator ChasePlayerRoutine()
+    {
+        while (true)
+        {
+            // 공격 발사 가능여부 판단
+            bool hitted = Physics.Raycast(transform.position, detected[0].transform.position - transform.position, out RaycastHit info, attackRange, shared.hitableLayerMask);
+
+            if (hitted && info.collider.gameObject.layer == shared.playerLayerIndex)
+            {
+                Debug.Log("공격");
+            }
+            else
+            {
+                agent.destination = detected[0].transform.position;
+            }
+
+            yield return shared.checkAttackPeriod;
         }
     }
 }
